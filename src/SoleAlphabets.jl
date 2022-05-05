6177977e7b81cced315b8795402575bccfccbbdd
paraandrea <andrea.paradiso.uni@gmail.com>
2022-05-05 14:46:27 +0200

    added propositions

--- src/SoleAlphabets.jl
+++ src/SoleAlphabets.jl
@@ -4,46 +4,44 @@ import IterTools
 
 abstract type AbstractProposition end
 
-abstract type AbstractBoundedProposition <: AbstractProposition end
+abstract type BoundedProposition <: AbstractProposition end
 
-abstract type SingleBoundedProposition <: AbstractProposition end
-abstract type DoubleBoundedProposition <: AbstractProposition end
+struct DoubleBoundedProposition <: BoundedProposition
 
-abstract type LeftBoundedProposition <: SingleBoundedProposition end
-abstract type RightBoundedProposition <: SingleBoundedProposition end
-
-struct BoundedProposition{T} <: AbstractBoundedProposition
-
-    left_bound::Union{Real,Nothing}
-    left_operator::Union{Function,Nothing}
+    left_bound::Real
+    left_operator::Function
     value::Real
-    right_operator::Union{Function,Nothing}
-    right_bound::Union{Real,Nothing}
+    right_operator::Function
+    right_bound::Real
 
-    function BoundedProposition(left_bound,left_operator,value,right_operator,right_bound)
+    function DoubleBoundedProposition(left_bound,left_operator,value,right_operator,right_bound)
 
         @assert left_operator(left_bound,value) "Invalid proposition"
         @assert right_operator(value,right_bound) "Invalid proposition"
 
-        return new{DoubleBoundedProposition}(
-        value,
-        left_bound,
-        right_bound,
-        left_operator,
-        right_operator
+        return new(
+            value,
+            left_bound,
+            right_bound,
+            left_operator,
+            right_operator
         )
 
     end
 
-    function BoundedProposition(value,operator,bound)
+end
+
+struct SingleBoundedProposition <: AbstractBoundedProposition
+
+    value::Real
+    operator::Function
+    bound::Real
+
+    function SingleBoundedProposition(value,operator,bound)
 
         @assert operator(value,bound) "Invalid proposition"
 
-        if operator(value,bound)
-            return new{RightBoundedProposition}(nothing,nothing,value,operator,bound)
-        else
-            return new{LeftBoundedProposition}(bound,operator,value,nothing,nothing)
-        end
+        return new(value,operator,bound)
 
     end
 
