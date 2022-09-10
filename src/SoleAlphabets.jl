9d12d0d303dfdc71a7693db6845410bcbded6d23
mauro-milella <milella_mauro@live.it>
2022-09-11 00:58:49 +0200

    is_proposition trait exported and dependencies updated

--- src/SoleAlphabets.jl
+++ src/SoleAlphabets.jl
@@ -1,11 +1,13 @@
 module SoleAlphabets
 
+using SoleTraits # is_proposition trait
+
 import Base.show
 import Base.Iterators
 
 import IterTools
 
-export Letter, LetterAlphabet
+export Letter, LetterAlphabet, is_proposition
 
 export SingleBoundedProposition, DoubleBoundedProposition
 export propositions
